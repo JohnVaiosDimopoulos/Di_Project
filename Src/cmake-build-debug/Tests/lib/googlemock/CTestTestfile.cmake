97da538e1f3744d4c226d10c839d72b10ffa73f5
supergohan <johnvaiosdim@yahoo.com>
2019-10-25 03:02:07 +0300

    New Repo initial Commit

--- Src/cmake-build-debug/Tests/lib/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/supergohan/Desktop/Di_Project/Src/Tests/lib/googlemock
-# Build directory: /home/supergohan/Desktop/Di_Project/Src/cmake-build-debug/Tests/lib/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
