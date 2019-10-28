312b98a052a1282048e1969c16edda1beb6b9440
Danae Roumelioti <sdi1600145@di.uoa.gr>
2019-10-28 11:45:45 +0200

    Added radix_sort file

--- Src/Initializer/radix_sort.c
+++ Src/Initializer/radix_sort.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 
-uint64_t getMax(uint64_t arr[], int n) 
-{ 
+uint64_t getMax(uint64_t arr[], int n) { 
     uint64_t mx = arr[0]; 
     for (int i = 1; i < n; i++) 
         if (arr[i] > mx) 
@@ -12,8 +11,7 @@ uint64_t getMax(uint64_t arr[], int n)
   
 // A function to do counting sort of arr[] according to 
 // the digit represented by exp. 
-void countSort(uint64_t arr[], int n, uint64_t exp) 
-{ 
+void countSort(uint64_t arr[], int n, uint64_t exp) { 
     uint64_t output[n]; // output array 
     int i, count[2] = {0}; 
   
@@ -27,8 +25,7 @@ void countSort(uint64_t arr[], int n, uint64_t exp)
         count[i] += count[i - 1]; 
   
     // Build the output array 
-    for (i = n - 1; i >= 0; i--) 
-    { 
+    for (i = n - 1; i >= 0; i--) { 
         output[count[ (arr[i]/exp)%2 ] - 1] = arr[i]; 
         count[ (arr[i]/exp)%2 ]--; 
     } 
@@ -41,14 +38,13 @@ void countSort(uint64_t arr[], int n, uint64_t exp)
   
 // The main function to that sorts arr[] of size n using  
 // Radix Sort 
-void radixsort(uint64_t arr[], int n) 
-{ 
+void radixsort(uint64_t arr[], int n) { 
     // Find the maximum number to know number of digits 
     uint64_t m = getMax(arr, n); 
   
     // Do counting sort for every digit. Note that instead 
-    // of passing digit number, exp is passed. exp is 10^i 
-    // where i is current digit number 
+    // of passing digit number, exp is passed. exp is 0x010000 * 2i 
+    // where i is current bit of the first byte 
     for (uint64_t exp = 0x010000; m/exp >= 1; exp *= 2) {
 //		printf("exp = %llu\n", exp);
         countSort(arr, n, exp); 
@@ -56,8 +52,7 @@ void radixsort(uint64_t arr[], int n)
 } 
   
 // A utility function to print an array 
-void print(uint64_t arr[], int n) 
-{ 
+void print(uint64_t arr[], int n) { 
     for (int i = 0; i < n; i++) 
 		printf("%llu ", arr[i]);
 	printf("\n");
@@ -65,7 +60,9 @@ void print(uint64_t arr[], int n)
   
 //sort regarding the first rightmost byte 
 int main() {
+//we have
 //0x201000, 0x200000, 0x0fffff, 0x5bc300 
+//we want
 //0x0fffff, 0x201000 , 0x200000, 0x5bc300
 
     uint64_t arr[] = {2101248, 2097152, 1048575, 6013696}; 
