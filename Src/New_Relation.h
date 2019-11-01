7ab12ec6dec02dae96b0cb9143ab37064a6f1826
Danae Roumelioti <sdi1600145@di.uoa.gr>
2019-11-01 15:58:24 +0200

    Added Final Relation

---
+++ Src/New_Relation.h
@@ -0,0 +1,10 @@
+#ifndef SORT_JOIN_NEW_RELATION_H
+#define SORT_JOIN_NEW_RELATION_H
+
+#include <stdio.h>
+#include <stdint.h>
+#include "Basis_Structs/Relation.h"
+
+void Copy_Relation(RelationPtr, RelationPtr, Histogram_Ptr, Psum_Ptr);
+
+#endif //SORT_JOIN_RELATION_CREATOR_H
---
+++ Src/run.sh
@@ -0,0 +1,1 @@
+./main -T1 ./Table_files/TableR -T2 ./Table_files/TableS && make clean
