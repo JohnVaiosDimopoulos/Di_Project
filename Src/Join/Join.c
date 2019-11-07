440fe01e359e1f65b6170a0777851d06b2b00878
supergohan <johnvaiosdim@yahoo.com>
2019-11-07 14:04:56 +0200

    refactoring

--- Src/Join/Join.c
+++ Src/Join/Join.c
@@ -37,23 +37,26 @@ Node_Ptr New_Node() {
 static List_Ptr Create_and_Initialize_List() {
   List_Ptr List = (List_Ptr)malloc(sizeof(struct Result_List));
   List->start = New_Node();
+  List->last = List->start;
   return List;
 }
 
 void Insert_Record(List_Ptr List, uint64_t id_1, uint64_t id_2) {
-  Node_Ptr Pnode = List->start;
-  while(Pnode->next)
-	  Pnode = Pnode->next;
+//  Node_Ptr Pnode = List->start;
+  Node_Ptr Pnode = List->last;
+//  while(Pnode->next)
+//	  Pnode = Pnode->next;
 
   if(Pnode->counter == LIST_SIZE) {
     Pnode->next = New_Node();
-	Pnode = Pnode->next;
+    Pnode = Pnode->next;
   }
+  List->last = Pnode;
 
   Pnode->Array[Pnode->counter][0] = id_1;
   Pnode->Array[Pnode->counter][1] = id_2;
 
-  Pnode->counter++; 
+  Pnode->counter++;
 }
 
 void Delete_List(List_Ptr List){
@@ -61,34 +64,37 @@ void Delete_List(List_Ptr List){
 }
 
 void Join(RelationPtr A, RelationPtr B) {
+  printf("JOIN\n\n");
   List_Ptr List = Create_and_Initialize_List();
 
   Tuple_Ptr A1, A2, B1, B2;
   A1 = A2 = A->tuples;
   B1 = B2 = B->tuples;
-  
+
   int i = 0;
   int cntA = 0;
   int cntB = 0;
+//  FILE *fp = fopen("../../../Desktop/output.txt", "w");
   while(1) {
     if(A2->element < B2->element) {
       cntA++;
       if(cntA == A->num_of_tuples)
-      	break;
+        break;
       A2++;
       A1 = A2;
       continue;
     } else if(A2->element > B2->element) {
       B2++;
       if(B1->element != B2->element) {
-      	B1 = B2;
+        B1 = B2;
       }
       continue;
     }
     if(A2->element == B2->element) {
       Insert_Record(List, A2->row_id, B2->row_id);
+//    fprintf(fp, "%d: Table1: %llu, Table2: %llu\n", i, A2->row_id, B2->row_id);
       i++;
-//      printf("%d:A (%llu %llu) ",i, A2->row_id, A2->element); 
+//      printf("%d:A (%llu %llu) ",i, A2->row_id, A2->element);
 //      printf("B (%llu %llu)\n", B2->row_id, B2->element);
     }
     B2++;
@@ -105,9 +111,8 @@ void Join(RelationPtr A, RelationPtr B) {
       }
     }
   }
-
-  FILE *fp;
-  Open_File_for_Write(&fp,"./Results/output");
+  printf("JOIN\n\n");
+  FILE *fp = fopen("./Results/output", "w");
   Print_List(List->start, 0, fp);
   fclose(fp);
 
